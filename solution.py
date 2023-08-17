257adc69e82a7693d3dfc7d32fe8bf480d971714
Jana Ristovska <76064707+ristovska1606@users.noreply.github.com>
2023-08-17 14:11:07 +0200

    formatted solution.py

--- solution.py
+++ solution.py
@@ -13,15 +13,19 @@ print(data.isnull().sum())
 sns.pairplot(data)
 
 #Linear reggresion
-model = LinearRegression()
-y = data['Price 2023']
-x = data.drop(['Product','Price 2023','Currency','Country'],axis=1)
-#60:40
-X_train,X_test,Y_train,Y_test = train_test_split(x,y,test_size=0.4)
-print("Training data: " + X_train.shape)
-print("Testing data: " + X_test.shape)
-model.fit(X_train,Y_train)
-y_pred = model.predict(X_test)
-print(y_pred)
-print('Accuracy of linear regression classifier on test set: {:.3f}'.format(r2_score(Y_test, y_pred)))
-# A value of 1 indicates that predictions are identical to the observed values;
+def calculateLinearReggression(){
+    model = LinearRegression()
+    y = data['Price 2023']
+    x = data.drop(['Product','Price 2023','Currency','Country'],axis=1)
+    #60:40
+    X_train,X_test,Y_train,Y_test = train_test_split(x,y,test_size=0.4)
+    print("Training data: " + X_train.shape)
+    print("Testing data: " + X_test.shape)
+    model.fit(X_train,Y_train)
+    y_pred = model.predict(X_test)
+    print(y_pred)
+    print('Accuracy of linear regression classifier on test set: {:.3f}'.format(r2_score(Y_test, y_pred)))
+    # A value of 1 indicates that predictions are identical to the observed values;
+}
+
+
