b56bdb3ec973ae3097eb8b60be0dc7623acff6de
Jana Ristovska <76064707+ristovska1606@users.noreply.github.com>
2023-08-18 16:45:37 +0200

    created KNNRegression and formatted solution.py

--- solution.py
+++ solution.py
@@ -7,6 +7,7 @@ from sklearn.metrics import r2_score
 from sklearn.tree import DecisionTreeRegressor
 import sklearn.tree as tree
 from sklearn.preprocessing import StandardScaler
+from sklearn import neighbors
 
 data = pd.read_csv('data/food_usa.csv') 
 print(data)
@@ -45,8 +46,9 @@ def calculateDecisionTreeRegression(x,y):
     regressor = DecisionTreeRegressor()
     regressor.fit(X_train, Y_train)
 
-    y_pred = regressor.predict(X_test)
-    print("Accuracy of decision tree regressior on test set: {:.3f}".format(regressor.score(X_test, Y_test)))
+    Y_pred = regressor.predict(X_test)
+
+    print("Accuracy of decision tree regressior on test set: {:.3f}".format(r2_score(Y_test, Y_pred)))
 
     
     # plt.figure(figsize=(10,10))
@@ -54,6 +56,23 @@ def calculateDecisionTreeRegression(x,y):
     #            max_depth=5, filled=True);
 
 
+def calculateKNNRegression(x,y):
+    #60:40
+    X_train,X_test,Y_train,Y_test = train_test_split(x,y,test_size=0.4)
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    model = neighbors.KNeighborsRegressor(n_neighbors = 5)
+    model.fit(X_train, Y_train)
+
+    Y_pred = model.predict(X_test)
+
+    print("Accuracy of knn regressior on test set: {:.3f}".format(r2_score(Y_test, Y_pred)))
+
 
 calculateLinearReggression(x,y)
 calculateDecisionTreeRegression(x,y)
+calculateKNNRegression(x,y)
